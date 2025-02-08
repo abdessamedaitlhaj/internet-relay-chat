0c1adc680bd96e3abb23399d86daf4ed4673b0c2
Anouar El harami <ael-hara@e3r11p2.1337.ma>
2025-02-08 20:57:50 +0100

    bot question

---
+++ bot/bot.cpp
@@ -0,0 +1,15 @@
+#include "question.hpp"
+#include "pool.hpp"
+#include <iostream>
+
+// int main ()
+
+// {
+
+// 	QuestionGame q;
+// 	    // if (q.levels.size() > 1 && q.levels[1].size() > 1) {
+//         std::cout << q.levels[0][1].problem << std::endl;
+//     // } else {
+//     //     std::cerr << "Level 2 or the second question in level 2 does not exist" << std::endl;
+//     // }
+// }
---
+++ bot/question.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+class QuestionGame {
+	private :
+		struct Question {
+			std::string problem;
+			// std::vector<std::string> answers;
+			char correct;
+		};
+		void  initializeLevel1();
+		void  initializeLevel2();
+		void  initializeLevel3();
+		void  initializeLevel4();
+	public :
+		std::vector<std::vector<Question> >levels;
+		QuestionGame();
+		Question & getRandomQuestion(int level);
+};
